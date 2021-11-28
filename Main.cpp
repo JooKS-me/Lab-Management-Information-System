c5e0b66208b6bd263d358a6ad7629ea7b6cdda54
zhukunshuai <jookunshuai@gmail.com>
2021-11-28 23:17:33 +0800

    init

---
+++ Main.cpp
@@ -0,0 +1,18 @@
+#include "View.cpp"
+
+using namespace std;
+
+int main() {
+    View view;
+    while (view.mainView());
+    system("clear");
+    cout << "________                          \n"
+            "\\_____  \\  ___  __  ____  _______ \n"
+            " /   |   \\ \\  \\/ /_/ __ \\ \\_  __ \\\n"
+            "/    |    \\ \\   / \\  ___/  |  | \\/\n"
+            "\\_______  /  \\_/   \\___  > |__|   \n"
+            "        \\/             \\/         " << endl;
+    int n;
+    cin >> n;
+}
+
